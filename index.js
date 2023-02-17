219ee79d86d3d0d96bd4f0acbc3e0deaeed3f0b2
shukri74 <shukri.mohamed@mail.com>
2023-02-17 16:25:41 +0000

    fixes

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var Hours = [9, 10, 11, 12, 13, 14, 15, 16, 17,]
 
+var SaveButton = $("<td>")
+
+SaveButton.addClass("saveBtn")
+
+SaveButton.addClass("btn")
+
+SaveButton.addClass("p-2")
+
 var ContainerEL = $(".container")
 
 var CurrentHour = moment().format("HH")
 
 $.each(Hours, function(){
 
-    var NewTimeblock = $("<div>")
-
-    NewTimeblock.addClass("time-block")
+    var NewTimeblock = $("<tr>")
     
     NewTimeblock.addClass("row")
 
@@ -17,23 +23,25 @@ $.each(Hours, function(){
 }) 
 
 
-for(var i = 0; i <= Hours.length; i++){
 
-if(i > CurrentHour) {
+for(var i = 0; i < Hours.length; i++){
 
-        ContainerEL.children().addClass("future")
-
-}
-
-if(i == CurrentHour) {
+if(Hours[i] == CurrentHour) {
 
     ContainerEL.children().addClass("present")
 
-}
+} 
 
-if(i < CurrentHour) {
+
+if (Hours[i] < CurrentHour) {
 
     ContainerEL.children().addClass("past")
+
+}
+
+else{
+
+    ContainerEL.children().addClass("future")
 
 }
 
